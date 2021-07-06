bd4cd70fc992aa95ecec80ad5a6a962d15a35e85
wanghuidi <841417348@qq.com>
2021-07-06 23:43:26 +0800

    修改host

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ def sign():
 
 headers = {
     'Connection': 'keep-alive',
-    'Host': 'we.cqu.pt',
+    'Host': 'we.cqupt.edu.cn',
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.143 '
                   'Safari/537.36 MicroMessenger/7.0.4.501 NetType/WIFI MiniProgramEnv/Windows WindowsWechat ',
     'content-type': 'application/json',
